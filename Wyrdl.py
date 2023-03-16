8170859798ed25aefe445948e0a8a3ffd6fae88b
Carman Tran <xLoveKawaii97@gmail.com>
2023-03-15 23:43:23 -0600

    added notes

--- Wyrdl.py
+++ Wyrdl.py
@@ -3,6 +3,9 @@ for guess_num in range(1,7):
     guess = input(f"\nGuess {guess_num}: ").upper()
     if guess == "SNAKE":
         print("Correct")
-        break
+        break #statement break out of loop early if user guesses the right word
+
+#Does not require explicit 'else' statement
+#Code will only continues if guesses are wrong
 
     print("Wrong")
